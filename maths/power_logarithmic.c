08de5bd0e9919290b9a1a43ce26b48068320c2a4
Leonardo Gama <leonardo.lgama@gmail.com>
2025-02-10 00:59:06 +0100

    Maths: Add power logarithmic

---
+++ maths/power_logarithmic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int power(int x,int y);
+
+int main(int argc,char **argv){
+	int result = power(atoi(argv[1]),atoi(argv[2]));
+	printf("%d\n", result);
+	return 0;
+}
+
+// Function to calculate x raised to the power of y using divide and conquer
+int power(int x,int y){
+    // Base case: any number raised to the power of 0 is 1
+	if(y == 0) {
+		return 1;
+	}
+    // If y is even, use the property (x^y) = (x^(y/2)) * (x^(y/2))
+	if(y % 2 == 0) {
+		return power(x, y / 2) * power(x, y / 2);
+	}
+    // If y is odd, use the property (x^y) = x * (x^((y-1)/2)) * (x^((y-1)/2))
+	return x * power(x, (y - 1) / 2) * power(x, (y - 1) / 2);
+}
