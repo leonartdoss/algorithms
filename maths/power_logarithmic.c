0f7d859f06ea0d9dbc3f341fcfc6e5c642cae66f
Leonardo Gama <leonardo.lgama@gmail.com>
2025-02-10 01:58:48 +0100

    Update power_logarithmic.c

--- maths/power_logarithmic.c
+++ maths/power_logarithmic.c
@@ -11,14 +11,16 @@ int main(int argc,char **argv){
 
 // Function to calculate x raised to the power of y using divide and conquer
 int power(int x,int y){
-    // Base case: any number raised to the power of 0 is 1
+	// Base case: any number raised to the power of 0 is 1
 	if(y == 0) {
 		return 1;
 	}
-    // If y is even, use the property (x^y) = (x^(y/2)) * (x^(y/2))
+	
+	// If y is even, use the property (x^y) = (x^(y/2)) * (x^(y/2))
 	if(y % 2 == 0) {
 		return power(x, y / 2) * power(x, y / 2);
 	}
-    // If y is odd, use the property (x^y) = x * (x^((y-1)/2)) * (x^((y-1)/2))
+	
+	// If y is odd, use the property (x^y) = x * (x^((y-1)/2)) * (x^((y-1)/2))
 	return x * power(x, (y - 1) / 2) * power(x, (y - 1) / 2);
 }
